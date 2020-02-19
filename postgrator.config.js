878660677a5ef89a2bd1f2ada1aedaeaad6a32a4
Zane Kuehn <zanekuehn@yahoo.com>
2020-02-19 09:30:07 -0600

    Added Migrations

--- postgrator.config.js
+++ postgrator.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-	migrationDirectory: 'migrations',
+	migrationDirectory: 'src/migrations',
 	driver: 'pg',
 	host: process.env.MIGRATION_DB_HOST,
 	port: process.env.MIGRATION_DB_PORT,
